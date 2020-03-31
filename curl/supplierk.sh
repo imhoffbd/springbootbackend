c37c9186aa5f508ca9bed1d996c68901f7bcbdc8
Bernd Imhoff <bernd@kunz-imhoff.de>
2020-03-31 16:13:06 +0200

    Service is running now

---
+++ curl/supplierk.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+source config.inc
+
+EXT=""
+if [ $# -gt 1  ]; then
+  EXT="/$2"
+fi
+
+curl -i http://10.211.55.5:31000/supplier
--- docker/dockerrun.sh
+++ docker/dockerrun.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -p 8080:8080 -e mongodb.connectstring='mongodb://172.17.0.1:27017/' -t mytest/springbootbackend
+docker run -p 8080:8080 -e mongodb.connectstring='mongodb://172.17.0.1:27017/' -t imhoffbd/springbootbackend
